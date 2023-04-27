fe8611a29ec09a8c27db2695d2122ba89469e57a
Muhammed Sayar <64076325+mrrsayarr@users.noreply.github.com>
2023-04-28 00:01:00 +0300

    Create StudentsView.cshtml.cs

---
+++ TestProject/Views/Units/StudentsView.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestProject.Views.Units
+{
+    public class StudentsViewModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
