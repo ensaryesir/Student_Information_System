52573cbc876322a0a6b769efbb59d25c3796ad15
Muhammed Sayar <64076325+mrrsayarr@users.noreply.github.com>
2023-04-28 00:00:46 +0300

    Update file

---
+++ TestProject/Controllers/UnitController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestProject.Controllers
+{
+    public class UnitController : Controller
+    {
+        public IActionResult StudentsView()
+        {
+            return RedirectToAction("StudentsView");
+        }
+    }
+}
