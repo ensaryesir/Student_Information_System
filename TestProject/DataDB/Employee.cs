b109f4305a7bdeedf250857b8ac9f787077bb185
Muhammed Sayar <64076325+mrrsayarr@users.noreply.github.com>
2023-05-01 19:27:06 +0300

    Update Employee.cs

--- TestProject/DataDB/Employee.cs
+++ TestProject/DataDB/Employee.cs
@@ -8,12 +8,16 @@ namespace TestProject.DataDB
     public partial class Employee
     {
         public int Id { get; set; }
+        public string StudentNo { get; internal set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Department { get; set; }
         public string Email { get; set; }
         public string Adress { get; set; }
-        //public object Gender { get; internal set; } // HATA ?
         public string PhoneNumber { get; set; }
+        public DateTime BirthDate { get; internal set; }
+        public DateTime RegistrationDate { get; internal set; }
+        public string Gender { get; internal set; }
+        public string IdentificationNumber { get; internal set; }
     }
 }
