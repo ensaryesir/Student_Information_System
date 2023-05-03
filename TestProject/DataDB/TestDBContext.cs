744a56ef25dd5a67a0ba33f79b07b1f3bcd08261
Ensar Yesir <yesirensar@gmail.com>
2023-05-03 12:26:59 +0300

    commit db duzeltme

--- TestProject/DataDB/TestDBContext.cs
+++ TestProject/DataDB/TestDBContext.cs
@@ -24,7 +24,7 @@ namespace TestProject.DataDB
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=LENOVO\\MSSQLSERVER02;Initial Catalog=SIS_DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+                optionsBuilder.UseSqlServer("Data Source=.;Initial Catalog=SIS_DB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
             }
         }
 
