a225634423264b505b4c152bf1ad2bec0461c192
Muhammed Sayar <64076325+mrrsayarr@users.noreply.github.com>
2023-05-09 17:23:20 +0300

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Student_Information_System.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
