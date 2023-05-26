6be75e3ca6fb42c73e381f2139af70265aef2880
Muhammed Sayar <muhammedsayar47@gmail.com>
2023-05-26 18:33:43 +0300

    V2.2 Update

--- SIS/Controllers/LoginController.cs
+++ SIS/Controllers/LoginController.cs
@@ -1,20 +1,83 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using SIS.Data;
+using SIS.Models;
+using System.Data;
 
 namespace SIS.Controllers
 {
     public class LoginController : Controller
     {
-        // GET: LoginController
-        public ActionResult Index()
+        // [Authorize] // Böylece bu sayfaya sadece oturumu açılmış kullanıcılar erişebilecektir
+        public IActionResult Index(Login model)
         {
-            return View();
+            using (var _connection = new SqlConnection("Data Source=.;Initial Catalog=SIS.Data;Integrated Security=True"))
+            {
+                _connection.Open();
+
+                if (ModelState.IsValid)
+                {
+                    // Authenticate the user
+                    var query = @"SELECT * FROM AuthorizedUser WHERE Username = @Username AND Password = @Password";
+
+                    using (var cmd = _connection.CreateCommand())
+                    {
+                        cmd.CommandText = query;
+                        cmd.Parameters.AddWithValue("@Username", model.Username);
+                        cmd.Parameters.AddWithValue("@Password", model.Password);
+
+                        var reader = cmd.ExecuteReader();
+
+                        if (reader.Read())
+                        {
+                            string role = reader["Authority"].ToString(); // Rol bilgisini veritabanından al
+
+                            // return RedirectToAction("Index", "Home");
+
+                            if (role.Equals("Student", StringComparison.OrdinalIgnoreCase))
+                            {
+                                // Student ise farklı bir sayfaya yönlendir
+                                return RedirectToAction("Index", "Home");
+
+                                //ModelState.AddModelError("", "Invalid username or password.");
+                                //return View(model);
+                            }
+                            else if (role.Equals("Academist", StringComparison.OrdinalIgnoreCase))
+                            {
+                                // Academist ise başka bir sayfaya yönlendir
+                                return RedirectToAction("Index", "Home");
+                            }
+                            else if (role.Equals("Student Affairs", StringComparison.OrdinalIgnoreCase))
+                            {
+                                // Student Affairs ise Home/Index sayfasına yönlendir
+                                return RedirectToAction("Index", "Home");
+                            }
+                        }
+
+                        else
+                        {
+                            // Geçersiz kullanıcı adı veya şifre hatası
+                            ModelState.AddModelError("", "Invalid username or password.");
+                            ViewBag.ErrorMessage = "Invalid username or password. Please try again.";
+                        }
+                    }
+                }
+
+                // Show an error message
+                ModelState.AddModelError("", "Invalid username or password.");
+                return View(model);
+            }
+
         }
 
-        // GET: LoginController/Details/5
-        public ActionResult Details(int id)
+        public IActionResult Logout()
         {
-            return View();
+            // Oturumu sonlandır
+            HttpContext.Session.Clear();
+
+            return RedirectToAction("Index", "Login");
         }
 
 
